cf8084e6bb2b76203850fb93a891785eb19a3c0d
Chris Parton <chrisparton1991@gmail.com>
2019-09-21 22:37:43 +1000

    Ensure fonts are loaded before initialising the slide deck

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+import WebFont from "webfontloader";
 import SlideDeck from "./slideDesk/SlideDeck";
+import AboutMeSlide from "./slides/intro/AboutMeSlide";
 import HobbiesSlide from "./slides/intro/HobbiesSlide";
 import TitleSlide from "./slides/intro/TitleSlide";
-import AboutMeSlide from "./slides/intro/AboutMeSlide";
 import ABriefHistorySlide from "./slides/part1/ABriefHistorySlide";
 import defaultTheme from "./theme/defaultTheme";
 
-new SlideDeck(defaultTheme).loadSlides(deck => [
-  new TitleSlide(deck),
-  new AboutMeSlide(deck),
-  new HobbiesSlide(deck),
-  new ABriefHistorySlide(deck)
-]).show();
+const theme = defaultTheme;
+
+WebFont.load({
+  custom: {
+    families: [theme.titleFontFamily],
+    urls: ['fonts/fonts.css']
+  },
+  active: start
+});
+
+function start() {
+  new SlideDeck(theme).loadSlides(deck => [
+    new TitleSlide(deck),
+    new AboutMeSlide(deck),
+    new HobbiesSlide(deck),
+    new ABriefHistorySlide(deck)
+  ]).show();
+}
